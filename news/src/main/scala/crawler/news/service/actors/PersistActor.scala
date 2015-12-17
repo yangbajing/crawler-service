361f147c7153997192009b52b387f90dfafef620
Yang Jing <yangbajing@gmail.com>
2015-12-17 16:11:34 +0800

    fix

--- news/src/main/scala/crawler/news/service/actors/PersistActor.scala
+++ news/src/main/scala/crawler/news/service/actors/PersistActor.scala
@@ -1,22 +1,25 @@
 package crawler.news.service.actors
 
 import akka.actor.Props
-import crawler.news.model.{NewsPage, NewsPageItem, NewsResult}
+import crawler.news.model.{NewsPage, NewsResult}
 import crawler.news.service.NewsDBRepo
 import crawler.util.actors.MetricActor
 
 /**
- * 持久化
- * Created by Yang Jing (yangbajing@gmail.com) on 2015-11-06.
- */
+  * 持久化
+  * Created by Yang Jing (yangbajing@gmail.com) on 2015-11-06.
+  */
 class PersistActor extends MetricActor {
   val dbRepo = new NewsDBRepo
 
   override val metricReceive: Receive = {
     case newsResult: NewsResult =>
       dbRepo.saveToSearchPage(newsResult)
-    case newsPage: NewsPage =>
-      dbRepo.saveToNewsPage(newsPage)
+
+      newsResult.news.foreach { item =>
+        val page = NewsPage(item.url, item.title, item.source, item.time, item.`abstract`, item.content.getOrElse(""))
+        dbRepo.saveToNewsPage(page)
+      }
   }
 
 }
