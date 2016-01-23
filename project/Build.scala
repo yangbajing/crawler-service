79d123f3e3d0df89b836dc10b830ec3f9efd01a9
Yang Jing <yangbajing@gmail.com>
2016-01-23 10:29:44 +0800

    update

--- project/Build.scala
+++ project/Build.scala
@@ -1,7 +1,11 @@
 import _root_.sbt.Keys._
 import _root_.sbt._
-import com.typesafe.sbt.SbtNativePackager.autoImport._
+import com.typesafe.sbt.SbtNativePackager.{Linux, Debian}
+import com.typesafe.sbt.packager.Keys._
 import com.typesafe.sbt.packager.archetypes.JavaServerAppPackaging
+import com.typesafe.sbt.packager.universal.UniversalPlugin.autoImport._
+import sbtassembly.AssemblyKeys._
+import sbtassembly.{MergeStrategy, PathList}
 
 object Build extends Build {
 
@@ -23,24 +27,49 @@ object Build extends Build {
   ///////////////////////////////////////////////////////////////
   // projects
   ///////////////////////////////////////////////////////////////
+  lazy val packageDebianProd = taskKey[File]("creates deb-prod package")
   lazy val appApi = Project("app-api", file("app-api"))
     .enablePlugins(JavaServerAppPackaging)
     .dependsOn(moduleSiteSearch % DependsConfigure, moduleNews % DependsConfigure, util % DependsConfigure)
     .settings(basicSettings: _*)
     .settings(
       description := "app-api",
-      //      assemblyJarName in assembly := "crawler-app.jar",
-      //      test in assembly := {},
-      //      assemblyMergeStrategy in assembly := {
-      //        case PathList("META-INF", "io.netty.versions.properties") => MergeStrategy.discard
-      //        case x =>
-      //          val oldStrategy = (assemblyMergeStrategy in assembly).value
-      //          oldStrategy(x)
-      //      },
-      mainClass := Some("crawler.app.Main"),
-      maintainer := "Jing Yang <jing.yang@socialcredits.cn, yangbajing@gmail.com>",
-      packageSummary := "Crawler High Search API",
-      packageDescription := "一个高级爬虫异步多线程实时API",
+
+      packageDescription := "一个高级异步多线程实时爬虫API",
+      mainClass in Compile := Some("crawler.app.Main"),
+      maintainer in Linux := "Jing Yang <jing.yang@socialcredits.cn, yangbajing@gmail.com>",
+      packageSummary in Linux := "Crawler High Search API",
+      daemonUser in Linux := "nobody",
+      bashScriptConfigLocation := Some("${app_home}/../conf/jvmopts"),
+      bashScriptExtraDefines += """addJava "-Dlogback.configurationFile=${app_home}/../conf/logback.xml"""",
+
+      //          |; bashScriptExtraDefines := Seq("addJava \"-Dconfig.file=${app_home}/../conf/application.conf -Dlogback.configurationFile=${app_home}/../conf/logback.xml\"")
+      addCommandAlias("packageProd",
+        """; clean
+          |; bashScriptExtraDefines += "addJava \"-Dconfig.file=${app_home}/../conf/application-test.conf -Dlogback.configurationFile=${app_home}/../conf/logback.xml\""
+          |; packageDebianProd
+        """.stripMargin),
+      packageDebianProd := {
+        bashScriptExtraDefines += """addJava "-Dconfig.file=${app_home}/../conf/application-test.conf -Dlogback.configurationFile=${app_home}/../conf/logback.xml""""
+        val output = baseDirectory.value / "package" / "deb-prod.deb"
+        val debianFile = (packageBin in Debian).value
+        IO.move(debianFile, output)
+        output
+      },
+
+//      assemblyJarName in assembly := "crawler-app.jar",
+//      mappings in Universal <<= (mappings in Universal, assembly in Compile) map { (mappings, fatJar) =>
+//        val filtered = mappings filter { case (file, name) => !name.endsWith(".jar") }
+//        filtered :+ (fatJar -> ("lib/" + fatJar.getName))
+//      },
+//      test in assembly := {},
+//      assemblyMergeStrategy in assembly := {
+//        case PathList("META-INF", "io.netty.versions.properties") => MergeStrategy.discard
+//        case x =>
+//          val oldStrategy = (assemblyMergeStrategy in assembly).value
+//          oldStrategy(x)
+//      },
+
       libraryDependencies ++= Seq(
         _akkaHttp)
     )
